cba0861ff740e3718867e4c055f1cd7268e922a7
DevJhojan <jhojan1806@gmail.com>
2026-04-18 16:10:10 -0500

    chore: add comment in vite.config.ts for clarity

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+// cambio
 // https://vite.dev/config/
 export default defineConfig({
   base: "/empresa-tecnologia/",
